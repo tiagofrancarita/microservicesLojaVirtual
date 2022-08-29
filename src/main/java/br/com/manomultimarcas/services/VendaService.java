d8d73f0c67c0f2e48d6497b2bca7afa6a44aa66f
Tiago França <tiagofranca.rita@gmail.com>
2022-08-28 23:52:45 -0300

    Exclusão lógica e consulta

--- src/main/java/br/com/manomultimarcas/services/VendaService.java
+++ src/main/java/br/com/manomultimarcas/services/VendaService.java
@@ -14,6 +14,17 @@ public class VendaService {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    public void exclusaoLogicaVenda(Long idVenda) {
+
+        String exclusaoLogicaVenda =
+                        "begin;"
+                        + " UPDATE vd_cp_loja_virtual SET excluido= true WHERE id =" + idVenda+" ; "
+                        + " commit;";
+
+        jdbcTemplate.execute(exclusaoLogicaVenda);
+
+    }
+
 
     public void deletaTotalVenda(Long idVenda) {
 
@@ -28,17 +39,6 @@ public class VendaService {
 
         jdbcTemplate.execute(exclusaoTotalVenda);
 
-
     }
-/*
-    @Modifying(flushAutomatically = true)
-    @Query(nativeQuery = true, value = "begin;"
-            + " UPDATE nota_fiscal_venda SET venda_compra_loja_virtualid= null WHERE venda_compra_loja_virtualid = :idVenda ; "
-            + " DELETE FROM nota_fiscal_venda WHERE venda_compra_loja_virtualid = :idVenda; "
-            + " DELETE FROM item_venda_loja WHERE venda_compra_loja_virtualid = :idVenda; "
-            + " DELETE FROM status_rastreio WHERE venda_compra_loja_virtualid = :idVenda; "
-            + " DELETE FROM vd_cp_loja_virtual WHERE id = :idVenda; "
-            + " commit; ")
-    void deletaVendaTotal(@Param("idVenda") Long idVenda);*/
 
 }
